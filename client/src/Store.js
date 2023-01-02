cdb2ee2f7e6ceda84aae3dea0733568a1d7adc2c
Javier Oliva <reivaj.e99@gmail.com>
2023-01-01 20:51:13 -0500

    signin fixes

--- client/src/Store.js
+++ client/src/Store.js
@@ -3,6 +3,9 @@ import {createContext, useReducer} from 'react';
 export const Store = createContext();
 
 const initialState = {
+    userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo'))
+    :null,
     cart: {
       cartItems: [],
       cartItems: localStorage.getItem('cartItems')
@@ -31,8 +34,11 @@ function reducer(state, action) {
                 (item) => item._id !== action.payload._id
             );
             localStorage.setItem('cartItems', JSON.stringify(cartItems));
-            return { ...state, cart: { ...state.cart, cartItems } };
-    }   
+            return { ...state, cart: { ...state.cart, cartItems } };}   
+        case 'USER_SIGNIN': {
+            return { ...state, userInfo: action.payload };}
+        case 'USER_SIGNOUT':
+            return { ...state, userInfo:null };
         default:
             return state;
     }
